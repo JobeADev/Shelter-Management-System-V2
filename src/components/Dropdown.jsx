4aa92588acaa8a93d4238df011283dd75957448b
Jobe <eloh2012@gmail.com>
2025-06-18 01:26:27 -0400

    added mobile view functionality, primarily to navbar and footer

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import { Link } from "@tanstack/react-router";
 import "./Dropdown.css";
 
 export default function Dropdown(props) {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
   return (
-    <ul className="dropdown-list w3-animate-opacity">
+    <ul
+      className={
+        click
+          ? "dropdown-list w3-animate-opacity clicked"
+          : "dropdown-list w3-animate-opacity"
+      }
+      onClick={handleClick}
+    >
       {props.props.map((i, index) => (
         <li key={index} className="dropdown-item">
-          <Link className="dropdown-link" to={i.path}>
+          <Link
+            className="dropdown-link"
+            to={i.path}
+            onClick={() => setClick(false)}
+          >
             {i.title}
           </Link>
         </li>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,63 +5,104 @@ import { AdoptItems, VolunteerItems, ResourcesItems } from "./DropdownItems";
 import "./NavBar.css";
 
 export default function NavBar() {
+  const [click, setClick] = useState(false);
   const [adoptDD, setAdoptDD] = useState(false);
   const [volunteerDD, setVolunteerDD] = useState(false);
   const [resourcesDD, setResourcesDD] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  function onMouseEnterDD(name) {
+    if (window.innerWidth <= 960) {
+      if (name === "adopt") {
+        setAdoptDD(false);
+      }
+      if (name === "volunteer") {
+        setVolunteerDD(false);
+      }
+      if (name === "resources") {
+        setResourcesDD(false);
+      }
+    } else {
+      if (name === "adopt") {
+        setAdoptDD(true);
+      }
+      if (name === "volunteer") {
+        setVolunteerDD(true);
+      }
+      if (name === "resources") {
+        setResourcesDD(true);
+      }
+    }
+  }
+
+  // function onMouseLeaveDD(name) {
+  //   if (name === "adopt") {
+  //     setAdoptDD(true);
+  //   }
+  //   if (name === "volunteer") {
+  //     setVolunteerDD(true);
+  //   }
+  //   if (name === "resources") {
+  //     setResourcesDD(true);
+  //   }
+  // }
 
   return (
     <nav className="nav-bar">
       <Link to="/" className="nav-logo">
         <i className="fa-solid fa-paw fa-3x"></i>
       </Link>
-      {/* <div className="menu-icon">
-        <i className="fas fa-bars fa-2x" />
-      </div> */}
-      <ul className="menu">
+      <div className="menu-icon" onClick={handleClick}>
+        <i className={click ? "fas fa-times" : "fas fa-bars"} />
+      </div>
+      <ul className={click ? "menu clicked" : "menu"}>
         <li
           className="nav-option"
-          onMouseEnter={() => setAdoptDD(true)}
+          onMouseEnter={() => onMouseEnterDD("adopt")}
           onMouseLeave={() => setAdoptDD(false)}
         >
-          <Link to="/adopt" className="nav-link">
+          <Link to="/adopt" className="nav-link" onClick={closeMobileMenu}>
             ADOPT
           </Link>
           {adoptDD ? <Dropdown props={AdoptItems} /> : null}
         </li>
         <li
           className="nav-option"
-          onMouseEnter={() => setVolunteerDD(true)}
+          onMouseEnter={() => onMouseEnterDD("volunteer")}
           onMouseLeave={() => setVolunteerDD(false)}
         >
-          <Link to="/volunteer" className="nav-link">
+          <Link to="/volunteer" className="nav-link" onClick={closeMobileMenu}>
             VOLUNTEER
           </Link>
           {volunteerDD ? <Dropdown props={VolunteerItems} /> : null}
         </li>
         <li
           className="nav-option"
-          onMouseEnter={() => setResourcesDD(true)}
+          onMouseEnter={() => onMouseEnterDD("resources")}
           onMouseLeave={() => setResourcesDD(false)}
         >
-          <Link to="/resources" className="nav-link">
+          <Link to="/resources" className="nav-link" onClick={closeMobileMenu}>
             RESOURCES
           </Link>
           {resourcesDD ? <Dropdown props={ResourcesItems} /> : null}
         </li>
         <li className="nav-option">
-          <Link to="/contact" className="nav-link">
+          <Link to="/contact" className="nav-link" onClick={closeMobileMenu}>
             CONTACT US
           </Link>
         </li>
         <li className="nav-option">
-          <Link to="/about" className="nav-link">
+          <Link to="/about" className="nav-link" onClick={closeMobileMenu}>
             ABOUT US
           </Link>
         </li>
-      </ul>
-      <ul className="menu-donate">
         <li className="donate-btn">
-          <Link to="/donate" className="donate-btn-link">
+          <Link
+            to="/donate"
+            className="donate-btn-link"
+            onClick={closeMobileMenu}
+          >
             DONATE
           </Link>{" "}
         </li>
